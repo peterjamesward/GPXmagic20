6198af2667fe6d7c1033ae7f17dea3ade6164eae
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-04-07 18:48:18 +0100

    Let us start with Track Points.

--- TODO.md
+++ TODO.md
@@ -1,11 +1,14 @@
 #TODOs
 
-TODO: https://package.elm-lang.org/packages/the-sett/tea-tree/latest/
-
-1. Basic structures and operations.
-2. Populate and test.
+1. Basic structures and operations. Keep it simple.
+2. Populate and test. (Parse GPX.)
+3. All trackpoint derivations in one pass.
 3. Simple rendering.
-4. Multiple views.
-5. New controls layout.
-6. Map integration.
-7. OAuth integration.
+4. Rendering with variable detail. (Try skip list of 12, with 1/2, 1/3, 1/4, 1/6, 1/12 options.)
+5. Multiple views. (Dynamically sized WebGL holes?)
+6. Graph by default. (Simple out and back falls out.)
+7. Message wrappers for edit messages.
+8. New controls layout.
+9. Map integration.
+9. OAuth integration.
+
---
+++ src/TrackPoint.elm
@@ -0,0 +1,42 @@
+module TrackPoint exposing (..)
+
+
+type GPXCoords
+    = GPXCoords
+
+
+type alias TrackPoint =
+    { gpxCoords : Point3d Length.Meters GPXCoords
+    , localCoords : Point3d Length.Meters LocalCoords
+    , effectiveDirection : Direction2d
+    , costMetric : Float
+    }
+
+
+type alias AtomicSpan =
+    { commences : TrackPoint
+    , concludes : TrackPoint
+    , segment : LineSegment3d Length.Meters LocalCoords
+    , effectiveDirection : Direction2d
+    , distanceAtStart : Float
+    , distanceAtEnd : Float
+    }
+
+
+type alias CompoundSpan =
+    { commences : TrackPoint
+    , concludes : TrackPoint
+    , segment : LineSegment3d Length.Meters LocalCoords
+    , effectiveDirection : Direction2d
+    , distanceAtStart : Float
+    , distanceAtEnd : Float
+    , contains : List Span
+    , box : BoundingBox3d Length.Meters LocalCoords
+    }
+
+
+type
+    Span
+    -- Unsure about this.
+    = AtomicSpan
+    | CompoundSpan
