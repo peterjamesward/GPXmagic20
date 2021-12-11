7a1b0ae6614df859357c39290a9ddbbe80372f90
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-12-11 22:40:53 +0000

    Outdoor map style, in Elm.

--- src/MapBox.elm
+++ src/MapBox.elm
@@ -11,6 +11,7 @@ import Mapbox.Expression as E exposing (false, float, int, str, true)
 import Mapbox.Layer as Layer
 import Mapbox.Source as Source
 import Mapbox.Style as Style exposing (Style(..))
+import Styles.Outdoors
 
 
 geojson =
@@ -51,7 +52,7 @@ hoveredFeatures =
 view _ =
     div [ style "width" "100vw", style "height" "100vh" ]
         [ map
-            [ maxZoom 5
+            [ maxZoom 20
 
             --, onMouseMove Hover
             --, onClick Click
@@ -59,65 +60,5 @@ view _ =
             , eventFeaturesLayers [ "changes" ]
             , hoveredFeatures []
             ]
-            (Style
-                { transition = Style.defaultTransition
-                , light = Style.defaultLight
-                , sources =
-                    [ Source.vectorFromUrl "composite"
-                        "mapbox://mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7,astrosat.07pz1g3y"
-                    , Source.geoJSONFromValue "changes" [] geojson
-                    ]
-                , misc =
-                    [ Style.name "light"
-                    , Style.defaultCenter <| LngLat 20.39789404164037 43.22523201923144
-                    , Style.defaultZoomLevel 1.5967483759772743
-                    , Style.sprite "mapbox://sprites/mapbox/light"
-                    , Style.glyphs "mapbox://fonts/mapbox/{fontstack}/{range}.pbf"
-                    ]
-                , layers =
-                    [ Layer.background "background"
-                        [ E.rgba 246 246 244 1 |> Layer.backgroundColor
-                        ]
-                    , Layer.fill "landcover"
-                        "composite"
-                        [ Layer.sourceLayer "landcover"
-                        , E.any
-                            [ E.getProperty (str "class") |> E.isEqual (str "wood")
-                            , E.getProperty (str "class") |> E.isEqual (str "scrub")
-                            , E.getProperty (str "class") |> E.isEqual (str "grass")
-                            , E.getProperty (str "class") |> E.isEqual (str "crop")
-                            ]
-                            |> Layer.filter
-                        , Layer.fillColor (E.rgba 227 227 227 1)
-                        , Layer.fillOpacity (float 0.6)
-                        ]
-                    , Layer.symbol "place-city-lg-n"
-                        "composite"
-                        [ Layer.sourceLayer "place_label"
-                        , Layer.minzoom 1
-                        , Layer.maxzoom 14
-                        , Layer.filter <|
-                            E.all
-                                [ E.getProperty (str "scalerank") |> E.greaterThan (int 2)
-                                , E.getProperty (str "type") |> E.isEqual (str "city")
-                                ]
-                        , Layer.textField <|
-                            E.format
-                                [ E.getProperty (str "name_en")
-                                    |> E.formatted
-                                    |> E.fontScaledBy (float 1.2)
-                                , E.formatted (str "\n")
-                                , E.getProperty (str "name")
-                                    |> E.formatted
-                                    |> E.fontScaledBy (float 0.8)
-                                    |> E.withFont (E.strings [ "DIN Offc Pro Medium" ])
-                                ]
-                        ]
-                    , Layer.fill "changes"
-                        "changes"
-                        [ Layer.fillOpacity (E.ifElse (E.toBool (E.featureState (str "hover"))) (float 0.9) (float 0.1))
-                        ]
-                    ]
-                }
-            )
+            Styles.Outdoors.style
         ]
